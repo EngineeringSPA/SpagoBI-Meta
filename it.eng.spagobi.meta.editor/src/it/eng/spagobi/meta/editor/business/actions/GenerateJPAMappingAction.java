64f101529589942615a2e0f0dfe29c4338fcefae
Marco Cortella <macortel@eng.it>
2015-06-05 15:35:13 +0000

    WIP: Update Physical Model
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Meta/trunk@24935 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- it.eng.spagobi.meta.editor/src/it/eng/spagobi/meta/editor/business/actions/GenerateJPAMappingAction.java
+++ it.eng.spagobi.meta.editor/src/it/eng/spagobi/meta/editor/business/actions/GenerateJPAMappingAction.java
@@ -6,16 +6,28 @@
  License, v. 2.0. If a copy of the MPL was not distributed with this file,
  You can obtain one at http://mozilla.org/MPL/2.0/.
  
-**/
+ **/
 package it.eng.spagobi.meta.editor.business.actions;
 
 import it.eng.spagobi.meta.editor.business.wizards.inline.GenerateJPAMappingWizard;
+import it.eng.spagobi.meta.editor.physical.actions.DeletePhysicalModelObjectAction;
+import it.eng.spagobi.meta.editor.physical.dialogs.DeleteElementsWarningDialog;
+import it.eng.spagobi.meta.initializer.PhysicalModelInitializer;
+import it.eng.spagobi.meta.model.ModelObject;
 import it.eng.spagobi.meta.model.business.BusinessModel;
 import it.eng.spagobi.meta.model.business.commands.ISpagoBIModelCommand;
 import it.eng.spagobi.meta.model.business.commands.generate.GenerateJPAMappingCommand;
 import it.eng.spagobi.meta.model.phantom.provider.BusinessRootItemProvider;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPart;
@@ -24,7 +36,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * @author cortella
- *
+ * 
  */
 public class GenerateJPAMappingAction extends AbstractSpagoBIModelAction {
 	private static Logger logger = LoggerFactory.getLogger(GenerateJPAMappingAction.class);
@@ -37,56 +49,66 @@ public class GenerateJPAMappingAction extends AbstractSpagoBIModelAction {
 	public GenerateJPAMappingAction(IWorkbenchPart workbenchPart, ISelection selection) {
 		super(GenerateJPAMappingCommand.class, workbenchPart, selection);
 	}
-	
+
 	/**
 	 * This executes the command.
 	 */
 	@Override
 	public void run() {
 		try {
-			/*
-			
-			PlatformUI.getWorkbench().getDisplay().syncExec(
-					new Runnable() {
-						public void run() {
-							IWorkbenchWindow window = PlatformUI
-							.getWorkbench()
-							.getActiveWorkbenchWindow();
-							logger.debug("WorkbenchWindow is [{}]",window);
-							if ( window != null ) {
-								IWorkbenchPage page = window.getActivePage();
-								logger.debug("WorkbenchPage is [{}]",page);
-								if ( page != null ) {
-									IEditorPart editor = page.getActiveEditor();
-									if ( editor != null ) {
-										IEditorInput input = editor.getEditorInput();
-										logger.debug("EditorInput is [{}]",input);
-										if ( input instanceof BusinessModelEditorInput ) {
-											BusinessModelEditorInput fileInput = (BusinessModelEditorInput) input;
-											logger.debug("Path is [{}]",fileInput.getResourceFileURI().toFileString());
-											IPath path = new Path(fileInput.getResourceFile().getAbsolutePath());
-											IFile file = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(path);
-											logger.debug("File is [{}]",file);
-											IProject activeProject = file.getProject();
-											logger.debug("Project is [{}]",activeProject);
-											logger.debug("Project name is [{}]",activeProject.getName());
-											logger.debug("Project path is [{}]",activeProject.getLocation().toOSString());
+			int returnCode = Window.CANCEL;
 
-										}
-									}
-								}					
-							}
+			BusinessModel businessModel = (BusinessModel) ((BusinessRootItemProvider) owner).getParentObject();
+
+			// First, check if there are any physical model objects marked as deleted
+			PhysicalModelInitializer physicalModelInitializer = new PhysicalModelInitializer();
+			List<ModelObject> markedElements = physicalModelInitializer.getElementsMarkedAsDeleted(businessModel.getPhysicalModel());
+			if (!markedElements.isEmpty()) {
+				DeleteElementsWarningDialog warningDialog = new DeleteElementsWarningDialog(markedElements);
+				warningDialog.create();
+				warningDialog.setBlockOnOpen(true);
+				returnCode = warningDialog.open();
+				if (returnCode == Window.OK) {
+					// execute a command for mass delete of elements marked as deleted
+					DeletePhysicalModelObjectAction deletePhysicalModelObjectAction = new DeletePhysicalModelObjectAction();
+					final Command deleteCommand = deletePhysicalModelObjectAction.createCommand(markedElements);
+					// this guard is for extra security, but should not be necessary
+					if (editingDomain != null && deleteCommand != null) {
+						// use up the command
+
+						ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(new Shell());
+						progressDialog.setCancelable(false);
+						try {
+							progressDialog.run(false, false, new IRunnableWithProgress() {
+								@Override
+								public void run(IProgressMonitor monitor) {
+									// Note: this is a non-UI Thread
+									monitor.beginTask("Deleting marked elements, please wait...", IProgressMonitor.UNKNOWN);
+									// doing task...
+
+									editingDomain.getCommandStack().execute(deleteCommand);
+
+									monitor.done();
+								}
+							});
+						} catch (InvocationTargetException e1) {
+							e1.printStackTrace();
+						} catch (InterruptedException e1) {
+							e1.printStackTrace();
 						}
-					} );
-			*/
-			
-			BusinessModel businessModel = (BusinessModel)((BusinessRootItemProvider)owner).getParentObject();
-			GenerateJPAMappingWizard wizard = new GenerateJPAMappingWizard(businessModel, editingDomain, (ISpagoBIModelCommand)command );
-	    	WizardDialog dialog = new WizardDialog(new Shell(), wizard);
-			dialog.create();
-	    	dialog.open();
-	    	
-		} catch(Throwable t) {
+					}
+
+				}
+			}
+
+			if (markedElements.isEmpty() || returnCode != Window.CANCEL) {
+				GenerateJPAMappingWizard wizard = new GenerateJPAMappingWizard(businessModel, editingDomain, (ISpagoBIModelCommand) command);
+				WizardDialog dialog = new WizardDialog(new Shell(), wizard);
+				dialog.create();
+				dialog.open();
+			}
+
+		} catch (Throwable t) {
 			t.printStackTrace();
 		}
 	}
--- it.eng.spagobi.meta.editor/src/it/eng/spagobi/meta/editor/olap/actions/CreateMondrianAction.java
+++ it.eng.spagobi.meta.editor/src/it/eng/spagobi/meta/editor/olap/actions/CreateMondrianAction.java
@@ -6,28 +6,39 @@
  License, v. 2.0. If a copy of the MPL was not distributed with this file,
  You can obtain one at http://mozilla.org/MPL/2.0/.
  
-**/
+ **/
 package it.eng.spagobi.meta.editor.olap.actions;
 
-import it.eng.spagobi.meta.editor.business.wizards.inline.NewQueryFileWizard;
 import it.eng.spagobi.meta.editor.olap.wizards.inline.NewMondrianFileWizard;
+import it.eng.spagobi.meta.editor.physical.actions.DeletePhysicalModelObjectAction;
+import it.eng.spagobi.meta.editor.physical.dialogs.DeleteElementsWarningDialog;
+import it.eng.spagobi.meta.initializer.PhysicalModelInitializer;
 import it.eng.spagobi.meta.model.Model;
+import it.eng.spagobi.meta.model.ModelObject;
 import it.eng.spagobi.meta.model.business.BusinessModel;
 import it.eng.spagobi.meta.model.business.commands.ISpagoBIModelCommand;
-import it.eng.spagobi.meta.model.business.commands.generate.CreateQueryCommand;
 import it.eng.spagobi.meta.model.olap.OlapModel;
 import it.eng.spagobi.meta.model.olap.commands.edit.generate.CreateMondrianCommand;
 import it.eng.spagobi.meta.model.phantom.provider.BusinessRootItemProvider;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.command.Command;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 
 /**
  * @author cortella
- *
+ * 
  */
 public class CreateMondrianAction extends AbstractSpagoBIModelAction {
 
@@ -36,29 +47,74 @@ public class CreateMondrianAction extends AbstractSpagoBIModelAction {
 	 * @param workbenchPart
 	 * @param selection
 	 */
-	public CreateMondrianAction(IWorkbenchPart workbenchPart,
-			ISelection selection) {
+	public CreateMondrianAction(IWorkbenchPart workbenchPart, ISelection selection) {
 		super(CreateMondrianCommand.class, workbenchPart, selection);
-	
+
 	}
-	
+
 	/**
 	 * This executes the command.
 	 */
 	@Override
 	public void run() {
 		try {
-			BusinessModel businessModel = (BusinessModel)((BusinessRootItemProvider)owner).getParentObject();
-			Model model = businessModel.getParentModel();
-			OlapModel olapModel = model.getOlapModels().get(0);
-					
-	    	NewMondrianFileWizard wizard = new NewMondrianFileWizard(olapModel, editingDomain, (ISpagoBIModelCommand)command );
-	    	wizard.init(PlatformUI.getWorkbench(), new StructuredSelection());
-	    	WizardDialog dialog = new WizardDialog(wizard.getShell(), wizard);
-			dialog.create();
-	    	dialog.open();
+			int returnCode = Window.CANCEL;
 
-		} catch(Throwable t) {
+			BusinessModel businessModel = (BusinessModel) ((BusinessRootItemProvider) owner).getParentObject();
+
+			// First, check if there are any physical model objects marked as deleted
+			PhysicalModelInitializer physicalModelInitializer = new PhysicalModelInitializer();
+			List<ModelObject> markedElements = physicalModelInitializer.getElementsMarkedAsDeleted(businessModel.getPhysicalModel());
+			if (!markedElements.isEmpty()) {
+				DeleteElementsWarningDialog warningDialog = new DeleteElementsWarningDialog(markedElements);
+				warningDialog.create();
+				warningDialog.setBlockOnOpen(true);
+				returnCode = warningDialog.open();
+				if (returnCode == Window.OK) {
+					// execute a command for mass delete of elements marked as deleted
+					DeletePhysicalModelObjectAction deletePhysicalModelObjectAction = new DeletePhysicalModelObjectAction();
+					final Command deleteCommand = deletePhysicalModelObjectAction.createCommand(markedElements);
+					// this guard is for extra security, but should not be necessary
+					if (editingDomain != null && deleteCommand != null) {
+						// use up the command
+
+						ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(new Shell());
+						progressDialog.setCancelable(false);
+						try {
+							progressDialog.run(false, false, new IRunnableWithProgress() {
+								@Override
+								public void run(IProgressMonitor monitor) {
+									// Note: this is a non-UI Thread
+									monitor.beginTask("Deleting marked elements, please wait...", IProgressMonitor.UNKNOWN);
+									// doing task...
+
+									editingDomain.getCommandStack().execute(deleteCommand);
+
+									monitor.done();
+								}
+							});
+						} catch (InvocationTargetException e1) {
+							e1.printStackTrace();
+						} catch (InterruptedException e1) {
+							e1.printStackTrace();
+						}
+					}
+
+				}
+			}
+
+			if (markedElements.isEmpty() || returnCode != Window.CANCEL) {
+				Model model = businessModel.getParentModel();
+				OlapModel olapModel = model.getOlapModels().get(0);
+
+				NewMondrianFileWizard wizard = new NewMondrianFileWizard(olapModel, editingDomain, (ISpagoBIModelCommand) command);
+				wizard.init(PlatformUI.getWorkbench(), new StructuredSelection());
+				WizardDialog dialog = new WizardDialog(wizard.getShell(), wizard);
+				dialog.create();
+				dialog.open();
+			}
+
+		} catch (Throwable t) {
 			t.printStackTrace();
 		}
 	}
